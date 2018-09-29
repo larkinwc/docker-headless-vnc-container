18c9a93a06da959b0f196d4347db330820eb6c3a
Larkin Williams-Capone <larkinwc@gmail.com>
2018-09-29 14:32:57 -0500

    Trying regular openjdk

--- src/ubuntu/install/jdk.sh
+++ src/ubuntu/install/jdk.sh
@@ -2,16 +2,9 @@
 ### every exit != 0 fails the script
 set -e
 
-echo "Install JDK 1.8"
+echo "Install OpenJDK"
 ### sudo apt-get install -y python-software-properties debconf-utils
-add-apt-repository -y ppa:webupd8team/java
 apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
-apt-get install -y oracle-java8-installer
-### set default Java
-apt-get install oracle-java8-set-default
-
+apt-get install -y default-jdk
 apt-get clean -y
 
-
-### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
