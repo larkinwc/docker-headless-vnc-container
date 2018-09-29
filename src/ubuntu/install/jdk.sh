874797d69bb5f7fb6473acd791e88b972af6d01a
Larkin Williams-Capone <larkinwc@gmail.com>
2018-09-29 13:43:21 -0500

    removed sudos

--- src/ubuntu/install/jdk.sh
+++ src/ubuntu/install/jdk.sh
@@ -4,12 +4,14 @@ set -e
 
 echo "Install JDK 1.8"
 ### sudo apt-get install -y python-software-properties debconf-utils
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-installer
+### set default Java
+apt-get install oracle-java8-set-default
+
 apt-get clean -y
 
-### set default Java
-sudo apt-get install oracle-java8-set-default
+
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
