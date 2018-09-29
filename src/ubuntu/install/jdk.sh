c6f8ad1757cc1701e0e27b5736e5a6bc7f973505
Larkin Williams-Capone <larkinwc@gmail.com>
2018-09-29 16:40:40 -0500

    adding set default

--- src/ubuntu/install/jdk.sh
+++ src/ubuntu/install/jdk.sh
@@ -10,4 +10,5 @@ add-apt-repository ppa:webupd8team/java -y
 apt-get update
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections 
 apt-get install -y oracle-java8-installer
+apt-get install oracle-java8-set-default
 apt-get clean -y
