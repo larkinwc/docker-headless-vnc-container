f196692027527800b15c76fbeee684de7db5961c
Larkin Williams-Capone <larkinwc@gmail.com>
2018-09-29 14:23:25 -0500

    Removed one last sudo

--- src/ubuntu/install/jdk.sh
+++ src/ubuntu/install/jdk.sh
@@ -6,7 +6,7 @@ echo "Install JDK 1.8"
 ### sudo apt-get install -y python-software-properties debconf-utils
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 apt-get install -y oracle-java8-installer
 ### set default Java
 apt-get install oracle-java8-set-default
