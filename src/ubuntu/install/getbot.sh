5265f771281ee5c6937ae00a9353f0f113ebfc7b
Larkin Williams-Capone <larkinwc@gmail.com>
2018-09-29 16:21:08 -0500

    Added rspeer launcher

--- src/ubuntu/install/getbot.sh
+++ src/ubuntu/install/getbot.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Downloading jars"
 ### sudo apt-get install -y python-software-properties debconf-utils
-wget --no-check-cert -O $home/TRiBot_Loader.jar https://tribot.org/bin/TRiBot_Loader.jar
-wget --no-check-cert -O $home/OSBuddy.jar http://cdn.rsbuddy.com/live/f/loader/OSBuddy.jar?x=10
-
+wget --no-check-cert -O $HOME/TRiBot_Loader.jar https://tribot.org/bin/TRiBot_Loader.jar
+wget --no-check-cert -O $HOME/OSBuddy.jar http://cdn.rsbuddy.com/live/f/loader/OSBuddy.jar?x=10
+wget --no-check-cert -O $HOME/rspeer-launcher.jar https://rspeer.org/rspeer-launcher.jar
