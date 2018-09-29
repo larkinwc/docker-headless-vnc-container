fbdbd58f607fd9306b3404b306c681c89d1876ed
Larkin Williams-Capone <larkinwc@gmail.com>
2018-09-29 15:46:57 -0500

    Added software properties so the 3rd party repo can be added

--- src/ubuntu/install/jdk.sh
+++ src/ubuntu/install/jdk.sh
@@ -5,6 +5,9 @@ set -e
 echo "Install OpenJDK"
 ### sudo apt-get install -y python-software-properties debconf-utils
 apt-get update
-apt-get install -y default-jdk
+apt-get install -y  software-properties-common
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections 
+apt-get install -y oracle-java8-installer
 apt-get clean -y
-
